6d03069100631cd4aff71ad7da28425338cfae63
comingaround <131857839+comingaround@users.noreply.github.com>
2024-02-10 18:47:45 +0200

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 import styles from "./about.module.css";
-import NavBar from "../components/NavBar/NavBar";
+import NavBar from "../components/General/NavBar/NavBar";
 
 export default function Dashboard() {
     return(
         <>
-        <h1 className={styles.dash}>Hello, Dashboard Page!</h1>
-        <NavBar />
-        <Link href="/">Home</Link>
+            <h1 className={styles.dash}>Hello, Dashboard Page!</h1>
+            <NavBar />
+            <Link href="/">Home</Link>
         </>
     );
   }
